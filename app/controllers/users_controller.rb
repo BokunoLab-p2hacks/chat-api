2f31006cab19cea41c9d66b7dfdd67a74a96ddeb
nyantama0616 <g2124029@fun.ac.jp>
2024-12-05 18:43:30 +0900

    POST /usersの実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,19 @@ class UsersController < ApplicationController
     @users = User.all
     render json: @users.map(&:info)
   end
+
+  def create
+    @user = User.new(params_create)
+    if @user.save
+      render json: @user.info
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def params_create
+    params.require(:user).permit(:name, :email)
+  end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -15,4 +15,31 @@ RSpec.describe "Users", type: :request do
       expect(response_body).to eq(User.all.map(&:info).map(&:stringify_keys))
     end
   end
+
+  describe "POST /users" do
+    before do
+      user = build(:user)
+      @params = { name: user.name, email: user.email }
+      
+      post "/users", params: { user: @params }
+    end
+
+    it "returns a 200 if the response succeeded" do
+      expect(response).to have_http_status(200)
+    end
+
+    it "creates a new user" do
+      expect(User.count).to eq(1)
+    end
+    
+    it "returns the new user#info" do
+      expect(response_body).to eq(User.first.info.stringify_keys)
+    end
+
+    it "returns a 422 if the user is invalid" do
+      @params[:name] = nil
+      post "/users", params: { user: @params }
+      expect(response).to have_http_status(422)
+    end
+  end
 end
