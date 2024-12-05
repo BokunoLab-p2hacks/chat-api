b7a89f539909d601cc855ccabf62627b26d1bc62
nyantama0616 <g2124029@fun.ac.jp>
2024-12-05 16:04:29 +0900

    userモデル作った

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  include Uuidable
+  
+  with_options presence: true do
+    validates :name
+    validates :email
+  end
 end
---
+++ spec/models/user.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe "validations" do
+    before do
+      @user = build(:user)
+    end
+
+    it "is valid with valid attributes" do
+      expect(@user).to be_valid
+    end
+
+    it "is not valid without a name" do
+      @user.name = nil
+      expect(@user).to_not be_valid
+    end
+
+    it "is not valid without an email" do
+      @user.email = nil
+      expect(@user).to_not be_valid
+    end
+  end
+
+  describe "uuid" do
+    it "is generated on create" do
+      user = create(:user)
+      expect(user.id).to be_present
+    end
+  end
+end
